93134251ef609808835b8476d0f352097b0f75c2
lucasborri83 <111891267+lucasborri83@users.noreply.github.com>
2022-09-01 13:14:04 -0300

    Added crossair

--- Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
+++ Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
@@ -10,21 +10,31 @@ ASExplosiveBarrel::ASExplosiveBarrel()
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
 	MeshComp->SetSimulatePhysics(true);
 	MeshComp->SetCollisionProfileName(UCollisionProfile::PhysicsActor_ProfileName);
-	MeshComp->OnComponentHit.AddDynamic(this, &ASExplosiveBarrel::OnActorHit);
+	
+
 	RootComponent = MeshComp;
 
 	ForceComp = CreateDefaultSubobject<URadialForceComponent>("ForceComp");
 	ForceComp->SetupAttachment(MeshComp);
+	ForceComp->SetAutoActivate(false); // Force component is doing some force in every tick by default.
 	ForceComp->Radius = 750.0f;
 	ForceComp->ImpulseStrength = 2000.0f;
-	ForceComp->bImpulseVelChange = true;
-	ForceComp->AddCollisionChannelToAffect(ECC_WorldDynamic);
+	ForceComp->bImpulseVelChange = true; // If true, it will ignore the mass of the objects.
+	ForceComp->AddCollisionChannelToAffect(ECC_WorldDynamic); // By deault the constructor of force component already added the other channels.
 }
 
 void ASExplosiveBarrel::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void ASExplosiveBarrel::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	// Better to add the 
+	MeshComp->OnComponentHit.AddDynamic(this, &ASExplosiveBarrel::OnActorHit);
 }
 
 void ASExplosiveBarrel::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
--- Source/ActionRoguelike/Public/SExplosiveBarrel.h
+++ Source/ActionRoguelike/Public/SExplosiveBarrel.h
@@ -28,6 +28,8 @@ protected:
 
 	virtual void BeginPlay() override;
 
+	virtual void PostInitializeComponents() override;
+
 	UFUNCTION()
 	void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 		FVector NormalImpulse, const FHitResult& Hit);
